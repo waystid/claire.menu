28f028f2d7dc84aacf038fc9a5ab0ef3e9319e4b
waystid <116369934+waystid@users.noreply.github.com>
2023-11-18 17:52:43 -0500

    theme.park

---
+++ MenuOptions/Submenu Registries/theme.park/theme.park.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+######################################################################
+# Title   : Install theme.park registry
+# By      : Lu Cipher, CE)
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by Claire™
+######################################################################
+
+# Include claire.func for all the awesome functions
+source /opt/claire/claire.menu/claire.func.sh
+
+# App Info
+app="theme.park"                                  # App Name
+title="theme.park registry"                         # Readable App Title
+image="ghcr.io/themepark-dev/theme.park"                         # Image and Tag
+volumes="    volumes:
+      - /config/\${APP_NAME:?err}:/config"
+tp_app=""                                       # Theme Park App Name
+porte="8089"                                    # External Port
+porti="80"                                    # Internal Port
+porte="4443"                                    # External Port
+porti="443"                                    # Internal Port
+extrapayload="    environment:
+      - PUID=1000          
+"                                 # Extra Payload to add to the Compose
+
+# Execute
+app

52d9b08603a759ba25ad2b7b3c0b1b81a424ba1e
waystid <116369934+waystid@users.noreply.github.com>
2023-11-18 18:29:14 -0500

    theme.park done

---
+++ MenuOptions/Submenu Registries/Docker Registry copy/theme.park.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+######################################################################
+# Title   : Install theme.park registry
+# By      : Lu Cipher, CEO
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by Claire™
+######################################################################
+
+# Include claire.func for all the awesome functions
+source /opt/claire/claire.menu/claire.func.sh
+
+# App Info
+app="theme-park"                                  # App Name
+title="theme.park registry"                                # Readable App Title
+image="ghcr.io/themepark-dev/theme.park"             # Image and Tag
+porte="8089"                                       # External Port
+porti="80"                                       # Internal Port
+porte2="4443"                                       # External Port
+porti2="443"                                       # Internal Port
+
+# App
+local_appcreate () {
+  msgbox "Installing $title..."
+  mkdir -p /config/$app && cd /config/$app
+  tee <<-EOF > .env
+APP_NAME=$app
+IMAGE=$image
+TP_APP=$tp_app
+PORTE=$porte
+PORTI=$porti
+PORTE2=$porte2
+PORTI2=$porti2
+
+EOF
+  tee <<-EOF > compose.yaml
+services:
+  service-name:
+    image: \${IMAGE:?err}
+    container_name: \${APP_NAME:?err}
+    network_mode: host
+    env_file:
+      - /config/.id.env
+      - /config/.timezone.env
+      - /config/.themepark.env
+    volumes:
+      - /config/\${APP_NAME:?err}:/config
+      - /data/torrents:/data/torrents"
+    ports:
+      - \${PORTE:?err}:${PORTI:?err}
+      - \${PORTE2:?err}:${PORTI2:?err}      
+    restart: unless-stopped
+    security_opt:
+      - apparmor:unconfined
+EOF
+  docker compose up -d --force-recreate
+}
+
+# List Links
+local_appfinalization () {
+  porte="8089"
+  check_ibradashy
+  logo
+  msgbox "All Done! Here is the link to $title:"
+  echo
+  ip=$(hostname -I | awk '{print $1}')
+  echo "$title: http://$ip:8089"
+  ibradashy
+  msgbox "You can also find $title on your IBRACORP Dashy website:"
+  echo "http://$ip:8086"
+  echo
+}
+
+# Execute
+
+# Local Docker
+  if [ "$app" != "registry" ]; then
+    #Update image file
+    update_image_file="/data/media/resources/claire/update_images"
+    
+    # Check if the line is already in the file
+    if ! grep -qF "$image" "$update_image_file"; then
+      sudo chmod u+w $update_image_file
+      echo "${image} | ${app}:${image_tag}" >> $update_image_file
+    else
+      echo "Line already exists in $update_image_file"
+    fi
+    # Execute local.docker function here
+    local.docker
+  else
+    echo "Skipping local.docker function because app is set to 'registry'"
+  fi
+
+local_appcreate
+local_appfinalization

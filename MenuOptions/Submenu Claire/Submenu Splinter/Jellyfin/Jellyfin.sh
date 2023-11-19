cef1fd11987fb7bdc44c4358b601f838f9fc342e
waystid <116369934+waystid@users.noreply.github.com>
2023-11-19 16:56:31 -0500

    Splinter & Ronin Directories

---
+++ MenuOptions/Submenu Claire/Submenu Splinter/Jellyfin/Jellyfin.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+######################################################################
+# Title   : Install Jellyfin
+# By      : DiscDuck, Taos15
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by Claire™
+######################################################################
+
+# Include claire.func for all the awesome functions
+source /opt/claire/claire.menu/claire.func.sh
+source /opt/claire/claire.menu/claire.func/sauce.sh
+
+# App Info
+app="jellyfin"                              # App Name
+title="Jellyfin"                            # Readable App Title
+image="lscr.io/linuxserver/jellyfin:latest" # Image and Tag
+
+# App
+local_appcreate () {
+  msgbox "Installing $title..."
+  mkdir -p /config/$app && cd /config/$app
+  tee <<-EOF > .env
+APP_NAME=$app
+IMAGE=$image
+TP_APP=$tp_app
+EOF
+  tee <<-EOF > compose.yaml
+services:
+  service-name:
+    image: \${IMAGE:?err}
+    container_name: \${APP_NAME:?err}
+    env_file:
+      - /config/.id.env
+      - /config/.timezone.env
+#    environment:
+#      - JELLYFIN_PublishedServerUrl=192.168.0.5 #optional
+    volumes:
+      - /config/jellyfin/config:/config
+      - /data:/media
+    ports:
+      - 8096:8096
+#      - 8920:8920 #optional
+#      - 7359:7359/udp #optional
+#      - 1900:1900/udp #optional
+    networks:
+      - $dockernet
+    restart: unless-stopped
+    security_opt:
+      - apparmor:unconfined
+
+networks:
+  $dockernet:
+    driver: bridge
+    external: true
+EOF
+  docker compose up -d --force-recreate
+}
+
+# List Links
+local_appfinalization () {
+  logo
+  msgbox "All Done! Here is the link to $title:"
+  echo
+  ip=$(hostname -I | awk '{print $1}')
+  echo "$title: http://$ip:8096"
+  echo
+}
+
+# Execute
+local.docker
+local_appcreate
+local_appfinalization
---
+++ MenuOptions/Submenu Claire/Submenu Splinter/Plex-Meta-Manager/Plex-Meta-Manager.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+######################################################################
+# Title   : Install Komga
+# By      : DiscDuck, Taos15
+# License : General Public License GPL-3.0-or-later
+# Another fine product brought to you by Claire™
+######################################################################
+
+# Include claire.func for all the awesome functions
+source /opt/claire/claire.menu/claire.func.sh
+source /opt/claire/claire.menu/claire.func/sauce.sh
+
+# App Info
+app="Plex-Meta-Manager"              # App Name
+title="Plex-Meta-Manager"            # Readable App Title
+image="meisnate12/plex-meta-manager" # Image and Tag
+volumes="    volumes:
+      - /config/\${APP_NAME:?err}:/config" # Volumes
+tp_app="Plex"                                   # Theme Park App Name
+# porte="8085"                                    # External Port
+# porti="8080"                                    # Internal Port
+extrapayload="" # Extra Payload to add to the Compose
+
+# Execute
+app
+
+sudo mkdir -p /config/Plex-Meta-Manager/assets
+curl -fLvo /config/Plex-Meta-Manager/config.yml https://raw.githubusercontent.com/meisnate12/Plex-Meta-Manager/master/config/config.yml.template

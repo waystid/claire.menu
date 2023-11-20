4dd77c609d434c159c183da214c9b01a1e92e5a2
code-server <waystid@me.com>
2023-11-20 08:38:37 +0000

    prepare splinter

---
+++ MenuOptions/Submenu Claire/Prepare Splinter/Prepare Splinter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the /config directory
+cd /config
+
+# Delete everything in /config except /plex and /code-server
+find . -mindepth 1 -maxdepth 1 ! -name "plex" ! -name "code-server" -exec rm -r {} \;
